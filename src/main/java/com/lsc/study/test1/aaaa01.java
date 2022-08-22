4501fc4e2b6de10fd059ca58104518ac9b021dbe
itlsc <1756332154@qq.com>
2022-08-22 14:18:11 +0800

    删除aaa01

--- src/main/java/com/lsc/study/test1/aaaa01.java
+++
@@ -1,9 +0,0 @@
-package com.lsc.study.test1;
-
-public class aaaa01 {
-
-    public static void main(String[] args) {
-        System.out.printf("11111");
-        System.out.printf("22222");
-    }
-}
